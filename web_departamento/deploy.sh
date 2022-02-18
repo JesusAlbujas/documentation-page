34e3ecfdc11d5fec2e65301748d9efbaa16d39c6
Inma Gijón <igijoninf@gmail.com>
2022-02-18 13:41:27 +0100

    Deploy

--- web_departamento/deploy.sh
+++ web_departamento/deploy.sh
@@ -15,19 +15,3 @@ cp LICENSE .vuepress/dist
 
 # cambiamos al directorio
 cd .vuepress/dist
-
-# Inicializamos el reposotorio
-git init
-
-# Añadimos los ficheros y hacemos comit en main
-git add -A
-date=`date +%Y-%m-%d_%H:%M:%S`
-git commit -m "deploy ${date}"
-git branch -M main
-
-# Nos conectamos a remoto y subimos
-git remote add origin https://github.com/CIFP-Virgen-de-Gracia/inf_com_dep_CIFP_VG.git
-git push -u -f origin main
-# git push -u origin main
-
-cd -
